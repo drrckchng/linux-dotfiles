98662e63b2f2d03ada2166adc8b4b3c4cf24da39
drrckchng <19944575+drrckchng@users.noreply.github.com>
2022-10-18 14:53:05 +0900

    Add more notes for future implementation

--- install.sh
+++ install.sh
@@ -18,10 +18,14 @@ sudo pacman -S neovim --noconfirm
 sudo pacman -S noto-fonts-cjk --noconfirm
 sudo pacman -S polybar --noconfirm
 # install icons
+# .local/share/fonts/awesome
+# .local/share/fonts/material-icons
 sudo pacman -S rofi --noconfirm
 sudo pacman -S stow --noconfirm
+sudo pacman -S zathura --noconfirm
 sudo pacman -S zsh --noconfirm
 
+yay -S nerd-fonts-complete --noconfirm
 yay -S picom-git --noconfirm
 yay -S visual-studio-code-bin --noconfirm
 
